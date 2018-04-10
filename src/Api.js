6078b007d403d948be61ac805b675ac7e43a2059
ethan ozelius <e.ozelius@gmail.com>
2018-04-10 10:28:42 -0400

    Api token sent as header instead of param

--- src/Api.js
+++ src/Api.js
@@ -1,21 +1,23 @@
 import axios from 'axios'
 
 export function fetchSources(){
-  const url = 'https://newsapi.org/v2/sources?apiKey=1b14114fcdff42a8b328c0a297d8a27f'
-  return axios.get(url)
+  alert(process.env.REACT_APP_NEWSAPI_KEY)
+
+  const url = 'https://newsapi.org/v2/sources'
+  return axios.get(url, { headers: { 'X-Api-Key': '29b629b0b9e94344a9e08037a0a7dc3b'}}) 
     .then((res) => {
       const sources = res.data.sources
-    	return sources
+      return sources
     })
     .catch((error) => {
-    	console.log(error)
+      console.log(error)
       return []
     })
 }
 
 export function fetchArticles(sources = ['abc-news']) {
   let url = 'https://newsapi.org/v2/top-headlines?sources='
-  const apiKey = '&apiKey=59519f47f5724e60b178a63e3b6c649b'
+  const apiKey = `&apiKey=${process.env.REACT_APP_NEWSAPI_KEY}`
 
   for(let i = 0; i < sources.length; i++){
     url += sources[i]
@@ -26,7 +28,7 @@ export function fetchArticles(sources = ['abc-news']) {
   }
   url += apiKey
 
-  return axios.get(url)
+  return axios.get(url, { headers: { 'X-Api-Key': '29b629b0b9e94344a9e08037a0a7dc3b' }})
     .then((res) => {
       const articles = res.data.articles
       return articles
